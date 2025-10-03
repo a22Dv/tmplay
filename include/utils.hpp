aebd4f96edb5715f1ce7138cd004ba393ff2e26a
a22Dv <a22dev.gl@gmail.com>
2025-10-04 07:45:49 +0800

    Added .clang-format file, class outline for Miniaudio-related structs/classes.

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <iostream>
+#include <stdexcept>
 
+#define ERR_LIST E(GENERIC_EXCEPTION, "A generic exception has been thrown.")
 namespace trm {
 
 inline void clearConsole() { std::cout << "\033[2J\033[H" << std::flush; }
@@ -9,6 +11,25 @@ inline void showError(const std::string &errMsg) {
     clearConsole();
     std::cout << "\e[0;31m" << "THROWN EXCEPTION:\n"
               << errMsg << "\e[0m" << std::endl;
+}
+
+enum class Error {
+    #define E(err, str) err,
+    ERR_LIST
+    #undef E
+    COUNT
+};
+
+constexpr const char* errMsg[static_cast<std::size_t>(Error::COUNT)] = {
+    #define E(err, str) str,
+    ERR_LIST
+    #undef E
+};
+
+inline void require(const bool cond, const Error err) {
+    if (!cond) {
+        throw std::runtime_error(errMsg[static_cast<std::size_t>(err)]);
+    }
 }
 
 } // namespace trm
---
+++ src/maudio.cpp
@@ -0,0 +1,23 @@
+#include "maudio.hpp"
+
+namespace trm {
+
+AudioDevice::AudioDevice() {
+    device.devConfig.playback.format = devSpec.format;
+    device.devConfig.playback.channels = devSpec.channels;
+    device.devConfig.sampleRate = devSpec.sampleRate;
+    device.devConfig.deviceType = devSpec.deviceType;
+    device.devConfig.dataCallback = device.callback;
+    device.devConfig.pUserData = static_cast<void*>(this);
+    ma_device_init(nullptr, &device.devConfig, &device.dev);
+}
+
+AudioDevice::~AudioDevice() {
+   ma_device_uninit(&device.dev);
+}
+
+void MaDevice::callback(ma_device *device, void *out, const void *in, unsigned int frames) {
+        
+}
+
+}
